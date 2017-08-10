327a2bbe557e335f5a43f2664c6e4270e273ebc7
enjoey01 <khmin1104@gmail.com>
2017-08-10 16:00:46 +0900

    update socket address

--- train/gym.py
+++ train/gym.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import random
 import mainForTraining
@@ -10,6 +11,6 @@ class Gym(object):
         if platform.system() == "Windows":
             server_address = ('localhost', random.randrange(2000,4000))
         elif platform.system() == "Linux":
-            server_address = '/data/socket_address/socket_%s'% random.randrange(0,2000)
+            server_address = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '/data/socket_address/socket_%s'% random.randrange(0,2000)))
         mainForTraining.main(server_address)
         return Environment(server_address)
